86190a292838a788d292d62243d2fbf41be1a1d5
daniel <daniel@caprica>
2010-03-29 13:26:49 -0300

    mudando o mestre pra ver se o malandro reclama

--- src/t/ATE.java
+++ src/t/ATE.java
@@ -1,9 +1,10 @@
 package t;
 
 public class ATE {
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		System.out.println("master");
 		System.out.println("bobo");
-		System.err.println("syserr"); 
+		System.err.println("syserr");
+		System.err.println("syserr4");
 	}
 }
