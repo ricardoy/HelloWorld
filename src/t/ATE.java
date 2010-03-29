a596219fb2606e7f95b7dfc9d6ed0bc633bf24e3
ricardo <ricardo@taurus.uplexis.com.br>
2010-03-29 12:54:51 -0300

    commit do master

--- src/t/ATE.java
+++ src/t/ATE.java
@@ -2,8 +2,7 @@ package t;
 
 public class ATE {
 	public static void main(String[] args) {
-		System.out.println("bobo");
-		System.out.println("oi");
-		System.err.println("erro");
+		System.out.println("master");
+
 	}
 }
