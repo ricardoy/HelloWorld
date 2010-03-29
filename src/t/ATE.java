8caa6f133ec3a08051a059e8e25026d2a71025c0
daniel <daniel@caprica>
2010-03-29 13:53:55 -0300

    long

--- src/t/ATE.java
+++ src/t/ATE.java
@@ -3,6 +3,7 @@ package t;
 public class ATE {
 	public static void main(final String[] args) {
 		final int a;
+		final long abc;
 		System.err.println("daniel");
 		System.err.println("syserr4");
 	}
