e7cc978ff5ea50a7f6a47b951c8ea4c0f29668b1
daniel <daniel@caprica>
2010-03-29 13:52:31 -0300

    String str

--- src/t/ATE.java
+++ src/t/ATE.java
@@ -2,8 +2,10 @@ package t;
 
 public class ATE {
 	public static void main(final String[] args) {
+		final String str;
 		System.out.println("bobo");
 		System.err.println("syserr");
 		System.err.println("toma essa ryuuga!");
+		System.err.println("toma de novo!");
 	}
 }
