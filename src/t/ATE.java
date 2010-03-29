4be751dac8a5f220f92ce1057047ed3959ce0787
ricardo <ricardo@taurus.uplexis.com.br>
2010-03-29 12:35:56 -0300

    merge de bobo e err

--- src/t/ATE.java
+++ src/t/ATE.java
@@ -2,12 +2,8 @@ package t;
 
 public class ATE {
 	public static void main(String[] args) {
-<<<<<<< .merge_file_xlfjwW
-
 		System.out.println("bobo");
-=======
 		System.out.println("oi");
 		System.err.println("erro");
->>>>>>> .merge_file_kk7vkT
 	}
 }
